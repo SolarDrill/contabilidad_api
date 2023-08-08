d7cfd38b777a1c0114e2f7a071aa2081dd72075b
Solar Drill <ramonsvnpp@gmail.com>
2023-08-07 21:39:40 -0400

    New files for Vercel deployment

--- contabilidad_api/config/settings/local.py
+++ contabilidad_api/config/settings/local.py
@@ -11,7 +11,7 @@ SECRET_KEY = env(
     default="433o5v2iEjM6aiQ0vFchO2BdTkPOpYormGcNj1ODIcnqHDNhNFsIvkVvGFidtvZC",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", ".vercel.app"]
 
 # CACHES
 # ------------------------------------------------------------------------------
